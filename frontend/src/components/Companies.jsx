a0e701cf2330594e428c169e013e7fe37a082205
anish-garg <anishgarg5914@gmail.com>
2023-10-08 02:23:17 +0530

    image error fixed

--- frontend/src/components/Companies.jsx
+++ frontend/src/components/Companies.jsx
@@ -4,8 +4,8 @@ const Companies = () => {
             <div className="flex justify-between items-center">
                 <img src="./prologis.png" alt="company" className="w-[12%]" />
                 <img src="./equinix.png" alt="company" className="w-[12%]" />
-                <img src=".tower.png" alt="company" className="w-[12%]" />
-                <img src=".realty.png" alt="company" className="w-[12%]" />
+                <img src="./tower.png" alt="company" className="w-[12%]" />
+                <img src="./realty.png" alt="company" className="w-[12%]" />
             </div>
         </>
     )
